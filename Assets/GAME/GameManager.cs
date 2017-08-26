01548a1285d321c4924a43c616faee65011fdb92
Bruno Franchini <hondokitsune@gmail.com>
2017-08-25 22:28:58 -0400

    Added logic to pause game.

--- Assets/GAME/GameManager.cs
+++ Assets/GAME/GameManager.cs
@@ -6,21 +6,51 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour
 {
     public bool recording;
+    private float initialFixedDeltaTime;
     private ReplaySystem replaySystem;
+    private bool isPaused;    
 
     void Start()
     {
+        initialFixedDeltaTime = Time.fixedDeltaTime;
         //unlock level 2
         PlayerPrefsManager.UnlockLevel(1);
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(1));
         Debug.Log(PlayerPrefsManager.IsLevelUnlocked(2));
     }
 
-	// Update is called once per frame
-	void Update ()
-	{
-	    recording = CrossPlatformInputManager.GetButton("Fire1");    
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        recording = CrossPlatformInputManager.GetButton("Fire1");
+
+
+        if (Input.GetKeyDown(KeyCode.P) && isPaused)
+        {
+            isPaused = false;
+            ResumeGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && !isPaused)
+        {
+            isPaused = true;
+            PauseGame();
+        }
+
+        print("Update");
+
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = initialFixedDeltaTime;
+    }
 
     public void SetRecording()
     {
@@ -31,4 +61,13 @@ public class GameManager : MonoBehaviour
     {
         recording = false;
     }
+
+    //todo: this is supposed to handle pausing when player navigates away from game
+    //todo: worry about this later 
+    //void OnApplicationPause(bool pauseStatus)
+    //{
+    //    isPaused = pauseStatus;
+    //    if (!pausedLastFrame && pauseStatus)
+    //        PauseGame();
+    //}
 }
